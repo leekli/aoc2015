d8e5046fdd11ef40f56579485159a7ab4f15d2da
leekli <li.kirkham@live.com>
2024-11-24 20:23:32 +0000

    Adds Day 6 Part 2 solution

--- day6/main.go
+++ day6/main.go
@@ -21,6 +21,11 @@ func main() {
 	p1Output := Part1(parsedInput)
 
 	fmt.Printf("Day 6, Part 1 Output: %d\n", p1Output)
+
+	// Part 2
+	p2Output := Part2(parsedInput)
+
+	fmt.Printf("Day 6, Part 2 Output: %d\n", p2Output)
 }
 
 func Part1(input [][]string) int {
@@ -40,7 +45,22 @@ func Part1(input [][]string) int {
 	return bulbOnCount
 }
 
-func Part2() {}
+func Part2(input [][]string) int {
+	rows := 1000
+	cols := 1000
+
+	grid := BuildHouseGrid(rows, cols)
+
+	for i := 0; i < len(input); i++ {
+		currentInstruction := input[i]
+
+		ExecuteInstruction_Part2(grid, currentInstruction)
+	}
+
+	bulbOnCount := CountBulbsSwitchedOn_Part2(grid)
+
+	return bulbOnCount
+}
 
 func readFileToString(filePath string) (string, error) {
 	content, err := os.ReadFile(filePath)
@@ -126,6 +146,24 @@ func ChangeBulbState(grid [][]int, instruction string, row int, col int) {
 	}
 }
 
+func ChangeBulbState_Part2(grid [][]int, instruction string, row int, col int) {
+	if instruction == "on" {
+		grid[row][col] += 1
+	}
+
+	if instruction == "off" {
+		grid[row][col] -= 1
+
+		if grid[row][col] < 0 {
+			grid[row][col] = 0
+		}
+	}
+
+	if instruction == "toggle" {
+		grid[row][col] += 2
+	}
+}
+
 func ExecuteInstruction_Part1(grid [][]int, instructions []string) {
 	instructionCode := instructions[0]
 
@@ -144,6 +182,24 @@ func ExecuteInstruction_Part1(grid [][]int, instructions []string) {
 	}
 }
 
+func ExecuteInstruction_Part2(grid [][]int, instructions []string) {
+	instructionCode := instructions[0]
+
+	splitFrom := strings.Split(instructions[1], ",")
+	splitTo := strings.Split(instructions[2], ",")
+
+	startRow, _ := strconv.Atoi(splitFrom[0])
+	finalRow, _ := strconv.Atoi(splitTo[0])
+	startCol, _ := strconv.Atoi(splitFrom[1])
+	finalCol, _ := strconv.Atoi(splitTo[1])
+
+	for i := startRow; i <= finalRow; i++ {
+		for j := startCol; j <= finalCol; j++ {
+			ChangeBulbState_Part2(grid, instructionCode, i, j)
+		}
+	}
+}
+
 func CountBulbsSwitchedOn(grid [][]int) int {
 	bulbOnCount := 0
 
@@ -152,6 +208,18 @@ func CountBulbsSwitchedOn(grid [][]int) int {
 			if grid[i][j] == 1 {
 				bulbOnCount++
 			}
+		}
+	}
+
+	return bulbOnCount
+}
+
+func CountBulbsSwitchedOn_Part2(grid [][]int) int {
+	bulbOnCount := 0
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			bulbOnCount += grid[i][j]
 		}
 	}
 
